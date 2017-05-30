e2fad033ca729371a68fbc18d0802ab450572613
Vidar Karlsen <vidar@karlsen.no>
2017-05-30 19:50:17 +0200

    Fix poudriere patch

--- ports-mgmt/poudriere/files/patch-src_share_poudriere_common.sh
+++ ports-mgmt/poudriere/files/patch-src_share_poudriere_common.sh
@@ -1,6 +1,6 @@
---- src/share/poudriere/common.sh.orig	2017-04-25 18:24:59 UTC
+--- src/share/poudriere/common.sh.orig	2017-05-30 17:47:06 UTC
 +++ src/share/poudriere/common.sh
-@@ -2398,7 +2398,7 @@ _real_build_port() {
+@@ -2403,7 +2403,7 @@ _real_build_port() {
  			JUSER=root
  			;;
  		extract)
@@ -9,7 +9,7 @@
  			chown -R ${JUSER} ${mnt}/wrkdirs
  			;;
  		configure) [ -n "${PORTTESTING}" ] && markfs prebuild ${mnt} ;;
-@@ -2419,12 +2419,12 @@ _real_build_port() {
+@@ -2424,7 +2424,7 @@ _real_build_port() {
  		checksum|*-depends|install-mtree) JUSER=root ;;
  		stage) [ -n "${PORTTESTING}" ] && markfs prestage ${mnt} ;;
  		install)
@@ -18,13 +18,7 @@
  			JUSER=root
  			[ -n "${PORTTESTING}" ] && markfs preinst ${mnt}
  			;;
- 		package)
--			max_execution_time=3600
-+			max_execution_time=14400
- 			if [ -n "${PORTTESTING}" ] &&
- 			    [ -z "${no_stage}" ]; then
- 				check_fs_violation ${mnt} prestage "${port}" \
-@@ -2438,7 +2438,7 @@ _real_build_port() {
+@@ -2443,7 +2443,7 @@ _real_build_port() {
  			fi
  			;;
  		deinstall)
