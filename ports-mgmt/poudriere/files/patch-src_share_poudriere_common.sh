30a0d25b737d092f3de099ab62eb2dc19c95f5f8
Vidar Karlsen <vidar@karlsen.no>
2017-08-01 13:00:16 +0200

    Fix max_execution_time patch

--- ports-mgmt/poudriere/files/patch-src_share_poudriere_common.sh
+++ ports-mgmt/poudriere/files/patch-src_share_poudriere_common.sh
@@ -1,16 +1,16 @@
---- src/share/poudriere/common.sh.orig	2017-08-01 10:34:16 UTC
+--- src/share/poudriere/common.sh.orig	2017-06-01 17:21:58 UTC
 +++ src/share/poudriere/common.sh
-@@ -2916,7 +2916,7 @@ _real_build_port() {
+@@ -2411,7 +2411,7 @@ _real_build_port() {
  			JUSER=root
  			;;
  		extract)
 -			max_execution_time=3600
 +			max_execution_time=14400
- 			if [ "${JUSER}" != "root" ]; then
- 				chown -R ${JUSER} ${mnt}/wrkdirs
- 			fi
-@@ -2939,7 +2939,7 @@ _real_build_port() {
- 		checksum|*-depends) JUSER=root ;;
+ 			chown -R ${JUSER} ${mnt}/wrkdirs
+ 			;;
+ 		configure) [ -n "${PORTTESTING}" ] && markfs prebuild ${mnt} ;;
+@@ -2432,12 +2432,12 @@ _real_build_port() {
+ 		checksum|*-depends|install-mtree) JUSER=root ;;
  		stage) [ -n "${PORTTESTING}" ] && markfs prestage ${mnt} ;;
  		install)
 -			max_execution_time=3600
@@ -18,7 +18,13 @@
  			JUSER=root
  			[ -n "${PORTTESTING}" ] && markfs preinst ${mnt}
  			;;
-@@ -2957,7 +2957,7 @@ _real_build_port() {
+ 		package)
+-			max_execution_time=7200
++			max_execution_time=14400
+ 			if [ -n "${PORTTESTING}" ] &&
+ 			    [ -z "${no_stage}" ]; then
+ 				check_fs_violation ${mnt} prestage "${port}" \
+@@ -2451,7 +2451,7 @@ _real_build_port() {
  			fi
  			;;
  		deinstall)
