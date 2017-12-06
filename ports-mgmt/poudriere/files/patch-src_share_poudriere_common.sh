612fbc190c8d65ea52247408a26f7c3cdce3c6d6
Vidar Karlsen <vidar@karlsen.no>
2017-12-06 18:04:04 +0100

    ports-mgmt/poudriere: version bump and colors
    - Bump to latest version (3.2.3)
    - Reimplement color changes from blackflow
    - Increase max_execution_time for everything, for slow boxes

--- ports-mgmt/poudriere/files/patch-src_share_poudriere_common.sh
+++ ports-mgmt/poudriere/files/patch-src_share_poudriere_common.sh
@@ -1,6 +1,6 @@
---- src/share/poudriere/common.sh.orig	2017-11-11 17:07:32 UTC
+--- src/share/poudriere/common.sh.orig	2017-12-06 16:41:50 UTC
 +++ src/share/poudriere/common.sh
-@@ -2988,7 +2988,7 @@ _real_build_port() {
+@@ -2989,7 +2989,7 @@ _real_build_port() {
  			JUSER=root
  			;;
  		extract)
@@ -9,7 +9,7 @@
  			if [ "${JUSER}" != "root" ]; then
  				chown -R ${JUSER} ${mnt}/wrkdirs
  			fi
-@@ -3012,12 +3012,12 @@ _real_build_port() {
+@@ -3013,7 +3013,7 @@ _real_build_port() {
  		checksum|*-depends) JUSER=root ;;
  		stage) [ -n "${PORTTESTING}" ] && markfs prestage ${mnt} ;;
  		install)
@@ -18,13 +18,7 @@
  			JUSER=root
  			[ -n "${PORTTESTING}" ] && markfs preinst ${mnt}
  			;;
- 		package)
--			max_execution_time=7200
-+			max_execution_time=57600
- 			if [ -n "${PORTTESTING}" ]; then
- 				check_fs_violation ${mnt} prestage \
- 				    "${originspec}" \
-@@ -3031,7 +3031,7 @@ _real_build_port() {
+@@ -3032,7 +3032,7 @@ _real_build_port() {
  			fi
  			;;
  		deinstall)
@@ -33,16 +27,16 @@
  			JUSER=root
  			# Skip for all linux ports, they are not safe
  			if [ "${PKGNAME%%*linux*}" != "" ]; then
-@@ -4900,7 +4900,7 @@ delete_old_pkg() {
+@@ -4875,7 +4875,7 @@ delete_old_pkg() {
  	_my_path mnt
  
  	if [ ! -d "${mnt}${PORTSDIR}/${origin}" ]; then
--		msg "${origin} does not exist anymore. Deleting stale ${pkg##*/}"
-+		msg "${COLOR_DELETE_PKG}${origin} does not exist anymore. Deleting stale ${pkg##*/}"
+-		msg "Deleting ${pkg##*/}: stale package: nonexistent origin ${origin}"
++		msg "${COLOR_DELETE_PKG}Deleting ${pkg##*/}: stale package: nonexistent origin ${origin}"
  		delete_pkg "${pkg}"
  		return 0
  	fi
-@@ -4937,7 +4937,7 @@ delete_old_pkg() {
+@@ -4910,7 +4910,7 @@ delete_old_pkg() {
  		# with a different origin.  Such as lang/perl5.20 vs
  		# lang/perl5.22 both with 'perl5' as PKGBASE.  A pkgclean
  		# would handle removing this.
@@ -51,7 +45,7 @@
  		delete_pkg "${pkg}"
  		return 0
  	fi
-@@ -4948,14 +4948,14 @@ delete_old_pkg() {
+@@ -4921,14 +4921,14 @@ delete_old_pkg() {
  	# version may show for a stale package that has been renamed.
  	# XXX: Check if the pkgname has changed and rename in the repo
  	if [ "${pkgbase}" != "${new_pkgbase}" ]; then
@@ -68,7 +62,16 @@
  		delete_pkg "${pkg}"
  		return 0
  	fi
-@@ -5071,7 +5071,7 @@ delete_old_pkg() {
+@@ -4936,7 +4936,7 @@ delete_old_pkg() {
+ 	if have_ports_feature FLAVORS; then
+ 		shash_get pkgname-flavor "${pkgname}" flavor || flavor=
+ 		if [ "${pkg_flavor}" != "${flavor}" ]; then
+-			msg "Deleting ${pkg##*/}: FLAVOR changed to '${flavor}' from '${pkg_flavor}'"
++			msg "${COLOR_DELETE_PKG_NEW}Deleting ${pkg##*/}: FLAVOR changed to '${flavor}' from '${pkg_flavor}'"
+ 			delete_pkg "${pkg}"
+ 			return 0
+ 		fi
+@@ -5053,7 +5053,7 @@ delete_old_pkg() {
  					*) ;;
  					esac
  				fi
@@ -77,7 +80,7 @@
  				delete_pkg "${pkg}"
  				return 0
  				;;
-@@ -5101,7 +5101,7 @@ delete_old_pkg() {
+@@ -5083,7 +5083,7 @@ delete_old_pkg() {
  		pkg_get_options compiled_options "${pkg}"
  
  		if [ "${compiled_options}" != "${current_options}" ]; then
@@ -86,7 +89,7 @@
  			if [ "${CHECK_CHANGED_OPTIONS}" = "verbose" ]; then
  				msg "Pkg: ${compiled_options}"
  				msg "New: ${current_options}"
-@@ -6617,7 +6617,7 @@ prepare_ports() {
+@@ -6583,7 +6583,7 @@ prepare_ports() {
  			listed_pkgnames | while read pkgname; do
  				pkg="${PACKAGES}/All/${pkgname}.${PKG_EXT}"
  				if [ -f "${pkg}" ]; then
@@ -95,3 +98,18 @@
  					delete_pkg_xargs "${delete_pkg_list}" \
  					    "${pkg}"
  				fi
+@@ -7325,10 +7325,10 @@ DRY_RUN=0
+ 
+ # Be sure to update poudriere.conf to document the default when changing these
+ : ${RESOLV_CONF="/etc/resolv.conf"}
+-: ${MAX_EXECUTION_TIME:=86400}         # 24 hours for 1 command (phase)
+-: ${NOHANG_TIME:=7200}                 # 120 minutes with no log update
+-: ${QEMU_MAX_EXECUTION_TIME:=345600}   # 4 days for 1 command (phase)
+-: ${QEMU_NOHANG_TIME:=21600}           # 6 hours with no log update
++: ${MAX_EXECUTION_TIME:=172800}         # 48 hours for 1 command (phase)
++: ${NOHANG_TIME:=14400}                 # 240 minutes with no log update
++: ${QEMU_MAX_EXECUTION_TIME:=691200}   # 8 days for 1 command (phase)
++: ${QEMU_NOHANG_TIME:=43200}           # 12 hours with no log update
+ : ${TIMESTAMP_LOGS:=no}
+ : ${ATOMIC_PACKAGE_REPOSITORY:=yes}
+ : ${KEEP_OLD_PACKAGES:=no}
--- ports-mgmt/poudriere/files/patch-src_share_poudriere_include_colors.sh
+++ ports-mgmt/poudriere/files/patch-src_share_poudriere_include_colors.sh
@@ -1,13 +1,13 @@
---- src/share/poudriere/include/colors.sh.orig	2017-11-11 17:07:32 UTC
+--- src/share/poudriere/include/colors.sh.orig	2017-12-06 16:39:36 UTC
 +++ src/share/poudriere/include/colors.sh
 @@ -79,6 +79,10 @@ if [ ${USE_COLORS} = "no" ]; then
  	COLOR_FAIL=
  	COLOR_PHASE=
  	COLOR_DRY_MODE=
-+	COLOR_DELETE_PKG=
-+	COLOR_DELETE_PKG_NEW=
-+	COLOR_DELETE_PKG_NEWDEP=
-+	COLOR_DELETE_PKG_OPTS=
++    COLOR_DELETE_PKG=
++    COLOR_DELETE_PKG_NEW=
++    COLOR_DELETE_PKG_NEWDEP=
++    COLOR_DELETE_PKG_OPTS=
  else
  
  	: ${D_LEFT:="${COLOR_BOLD}[${COLOR_RESET}"}
@@ -15,10 +15,10 @@
  	: ${COLOR_FAIL:=${COLOR_RED}}
  	: ${COLOR_PHASE:=${COLOR_LIGHT_MAGENTA}}
  	: ${COLOR_DRY_MODE:=${COLOR_GREEN}}
-+	: ${COLOR_DELETE_PKG:=${COLOR_RED}}
-+	: ${COLOR_DELETE_PKG_NEW:=${COLOR_GREEN}}
-+	: ${COLOR_DELETE_PKG_NEWDEP:=${COLOR_LIGHT_GREEN}}
-+	: ${COLOR_DELETE_PKG_OPTS:=${COLOR_YELLOW}}
++    : ${COLOR_DELETE_PKG:=${COLOR_RED}}
++    : ${COLOR_DELETE_PKG_NEW:=${COLOR_GREEN}}
++    : ${COLOR_DELETE_PKG_NEWDEP:=${COLOR_LIGHT_GREEN}}
++    : ${COLOR_DELETE_PKG_OPTS:=${COLOR_YELLOW}}
  fi
  
  colorize_job_id() {
